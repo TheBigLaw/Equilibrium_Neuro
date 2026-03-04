4b961f0e83a47db7cd8f93ef0e3715cdabf3b74e
André Marques <111473714+TheBigLaw@users.noreply.github.com>
2026-03-04 10:37:46 -0300

    Update script.js

--- Correcao_testes/SRS2/idade-escolar-feminino/script.js
+++ Correcao_testes/SRS2/idade-escolar-feminino/script.js
@@ -2,7 +2,7 @@
 /**
  * TROQUE SOMENTE ISSO EM CADA PASTA
  */
-const FORM_KEY = "adulto_autorrelato";
+const FORM_KEY = "idade_escolar_feminino";
 // Exemplos:
 // "pre_escolar"
 // "idade_escolar_feminino"
