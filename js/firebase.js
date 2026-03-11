c869f5ef77e29cb2b58066da25bfc6cb97215188
André Marques <111473714+TheBigLaw@users.noreply.github.com>
2026-03-11 11:13:35 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCsqUBa4-_PhfbCUIaPyNRQfXLNfVEuDms",
@@ -11,4 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const storage = getStorage(app);
